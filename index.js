bf7375639eea7e6c0fcec98902c88c6a51a15137
Adi <adi@tunity.com>
2017-06-11 21:58:16 +0300

    Adding a test for size compared to regular bitmap

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-var Redis = require('ioredis')
+var Redis = require('ioredis'),
+  _ = require('underscore')
 
 function RedisSparseBitmap(options) {
   this.redis = new Redis(options)
-  this.options = { chunk_size:32678}
+  this.options = {
+    chunk_size: 32678
+  }
 }
 
-RedisSparseBitmap.prototype.setbit = function(key,offset,value , callback) {
-  var chunk_offset = offset % this.options.chunk_size
-  var chunk_id = (offset - chunk_offset) / this.options.chunk_size
+RedisSparseBitmap.prototype.setbit = function(key, offset, value, callback) {
 
-  var sub_key = key + ':' + chunk_id
-  this.redis.setbit(sub_key,chunk_offset,value,callback)
+  var self = this
+
+  if (_.isArray(offset)) {
+
+    var p = self.redis.pipeline()
+
+    _.each(offset, function(_offset) {
+      var chunk_offset = _offset % self.options.chunk_size
+      var chunk_id = (_offset - chunk_offset) / self.options.chunk_size
+      var sub_key = key + ':' + chunk_id
+
+      p.setbit(sub_key, chunk_offset, value)
+    })
+
+    p.exec(callback)
+
+
+  } else {
+    var chunk_offset = offset % self.options.chunk_size
+    var chunk_id = (offset - chunk_offset) / self.options.chunk_size
+    var sub_key = key + ':' + chunk_id
+
+    self.redis.setbit(sub_key, chunk_offset, value, callback)
+  }
 }
 
 
--- test/test.js
+++ test/test.js
@@ -1,25 +1,69 @@
 var Redis = require('ioredis'),
   should = require('should'),
-  RedisSparseBitmap = require('../index'), _ = require('underscore')
+  RedisSparseBitmap = require('../index'),
+  _ = require('underscore')
 
 describe('nijinsky', function() {
 
+  var redis
 
-  it('should create sparse sub key', function(done) {
-    var redis = new Redis();
+  function getCurrentMemoryBytes(callback) {
+    redis.info('memory', function(err, memory) {
+      if (err) callback(err)
+
+      callback(null, parseInt(memory.split('\n')[1].split(':')[1]))
+
+    })
+  }
+
+  before(function() {
+    redis = new Redis()
+  })
+
+  beforeEach(function(done) {
+    redis.flushdb(done)
+  })
+
+  it('should take less space then regular bitmap', function(done) {
     var redis_sb = new RedisSparseBitmap()
 
-    var chunk_size = redis_sb.options.chunk_size
-    id = _.random(4294967296)
+    getCurrentMemoryBytes(function(err, memory) {
+      if (err) done(err)
 
-    expected_chunk_id = (id - (id % chunk_size)) /  chunk_size;
+      var initial_memory = memory
 
-    redis_sb.setbit('event' , id , 1 , function(err){
-      redis.exists('event' + ':' + expected_chunk_id , function(err,exists){
-        if(err) done(err)
-        exists.should.eql(1);
-        done()
+      var max_offset = 4294967296
+
+      ids = _.times(10, function() {
+        return _.random(max_offset)
+      })
+
+      var p = redis.pipeline()
+
+      _.each(ids, function(id) {
+        p.setbit('event0', id, 1)
+      })
+
+      p.exec(function(err) {
+        if (err) done(err)
+
+        getCurrentMemoryBytes(function(err, memory) {
+          if (err) done(err)
+          var bitmap_memory = memory - initial_memory;
+
+          redis_sb.setbit('event1', ids, 1, function(err) {
+            if (err) done(err)
+
+            getCurrentMemoryBytes(function(err, memory) {
+              if (err) done(err)
+              var sparse_bitmap_memory = memory - bitmap_memory;
+
+              sparse_bitmap_memory.should.be.lessThan(bitmap_memory / 100)
+              done()
+            })
+          })
+        })
       })
     })
-  });
+  })
 })
