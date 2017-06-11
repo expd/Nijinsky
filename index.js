eb6150fcbd305d20aed982b55a941fbfebfc80b3
Adi <adi@tunity.com>
2017-06-11 22:06:03 +0300

    Adding internal support for pipeline

--- index.js
+++ index.js
@@ -12,28 +12,29 @@ RedisSparseBitmap.prototype.setbit = function(key, offset, value, callback) {
 
   var self = this
 
-  if (_.isArray(offset)) {
+  var index_sub_key = key + ':_'
 
-    var p = self.redis.pipeline()
+  var p = self.redis.pipeline()
 
-    _.each(offset, function(_offset) {
-      var chunk_offset = _offset % self.options.chunk_size
-      var chunk_id = (_offset - chunk_offset) / self.options.chunk_size
-      var sub_key = key + ':' + chunk_id
-
-      p.setbit(sub_key, chunk_offset, value)
-    })
-
-    p.exec(callback)
-
-
-  } else {
-    var chunk_offset = offset % self.options.chunk_size
-    var chunk_id = (offset - chunk_offset) / self.options.chunk_size
+  var _setbit = function(_offset) {
+    var chunk_offset = _offset % self.options.chunk_size
+    var chunk_id = (_offset - chunk_offset) / self.options.chunk_size
     var sub_key = key + ':' + chunk_id
 
-    self.redis.setbit(sub_key, chunk_offset, value, callback)
+    p.setbit(sub_key, chunk_offset, value)
+    p.setbit(index_sub_key, chunk_id, 1)
   }
+
+  if (_.isArray(offset)) {
+    _.each(offset, _setbit)
+
+  } else {
+    _setbit(offset)
+
+  }
+
+  p.exec(callback)
+
 }
 
 
